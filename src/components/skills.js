1908b5601644ef560887c84128c585d787d8861e
UuqV <krlovell017@gmail.com>
2019-07-05 18:27:23 -0400

    Cute animations

--- src/components/skills.js
+++ src/components/skills.js
@@ -16,15 +16,23 @@ class skills extends Component {
         <h1>Built With</h1>
         <div className={skillstyles.skillsBox__holder}>
           <div className={skillstyles.skillsBox__holder__skill}>
-            <img src={jsIcon} width="200px" height="200px" alt="Js Icon" />
+            <img
+              src={jsIcon}
+              width="200px"
+              height="200px"
+              alt="Js Icon"
+              data-aos="fade-right"
+            />
             <h1>Javascript</h1>
           </div>
           <div className={skillstyles.skillsBox__holder__skill}>
             <img
               src={reactIcon}
               className={skillstyles.rotate}
-              width="300px"
+              width="200px"
+              height="150px"
               alt="React Icon"
+              data-aos="fade-right"
             />
             <h1>React</h1>
           </div>
@@ -34,19 +42,38 @@ class skills extends Component {
               width="200px"
               height="200px"
               alt="Gatsby Icon"
+              data-aos="fade-right"
             />
             <h1>Gatsby</h1>
           </div>
           <div className={skillstyles.skillsBox__holder__skill}>
-            <img src={sass} width="200px" height="200px" alt="SASS Icon" />
+            <img
+              src={sass}
+              width="200px"
+              height="200px"
+              alt="SASS Icon"
+              data-aos="fade-right"
+            />
             <h1>Sass</h1>
           </div>
           <div className={skillstyles.skillsBox__holder__skill}>
-            <img src={nodeIcon} width="200px" height="200px" alt="Node Icon" />
+            <img
+              src={nodeIcon}
+              width="200px"
+              height="200px"
+              alt="Node Icon"
+              data-aos="fade-right"
+            />
             <h1>NodeJs</h1>
           </div>
           <div className={skillstyles.skillsBox__holder__skill}>
-            <img src={graphql} width="200px" height="200px" alt="GQL Icon" />
+            <img
+              src={graphql}
+              width="200px"
+              height="200px"
+              alt="GQL Icon"
+              data-aos="fade-right"
+            />
             <h1>GraphQL</h1>
           </div>
           <div className={skillstyles.skillsBox__holder__skill}>
@@ -55,18 +82,28 @@ class skills extends Component {
               width="200px"
               height="200px"
               alt="Contentful Icon"
+              data-aos="fade-right"
             />
             <h1>Contentful</h1>
           </div>
-          <div
-            className={skillstyles.skillsBox__holder__skill}
-            data-aos="fade-up-left"
-            data-aos-easing="linear"
-            data-aos-duration="5000"
-            data-aos-delay="300"
-          >
-            <img src={heartIcon} width="200px" alt="Heart Icon" />
-            <h1>& Love</h1>
+          <div className={skillstyles.skillsBox__holder__skill}>
+            <img
+              src={heartIcon}
+              width="200px"
+              alt="Heart Icon"
+              data-aos="fade-left"
+              data-aos-easing="linear"
+              data-aos-duration="5000"
+              data-aos-delay="300"
+            />
+            <h1
+              data-aos="fade-bottom"
+              data-aos-easing="linear"
+              data-aos-duration="5000"
+              data-aos-delay="300"
+            >
+              & Love
+            </h1>
           </div>
         </div>
       </div>
